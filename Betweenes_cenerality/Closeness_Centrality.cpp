f1e14e245cc5b736bb5b5bb6946c7f1aa1063210
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2019-07-03 11:39:15 +0200

    Create Closeness_Centrality.cpp

---
+++ Betweenes_cenerality/Closeness_Centrality.cpp
@@ -0,0 +1,18 @@
+void Closeness_Centrality(vector<vector<int>> adj_matrix, int numOfver, int Src,long double &value )
+{
+	int* distance = new int[numOfver];
+	bool* ShortpassSet = new bool[numOfver];
+	int sumpass = 0;
+	 value = (double) (numOfver - 1);
+	for (int i = 0; i < numOfver; i++)
+	{
+		distance[i] = int_max;
+		ShortpassSet[i] = false;
+	}
+	Dijkstra(adj_matrix, distance, ShortpassSet, numOfver, Src);
+	ta5zeen(distance , numOfver , Src);
+	
+	delete[] distance;
+	delete[] ShortpassSet;
+ 
+}

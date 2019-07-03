28a1bc04dc36ba12ed989ccebc083ce9b89e4f20
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2019-07-03 11:39:55 +0200

    Create short_path.cpp

---
+++ Betweenes_cenerality/short_path.cpp
@@ -0,0 +1,14 @@
+int minDistance(bool ShortpassSet[], int distance[], int numOfver)
+{
+	int indexOfmin;
+	int min = int_max;
+	for (int i = 0; i < numOfver; i++)
+	{
+		if (distance[i] < min && ShortpassSet[i] == false)
+		{
+			min = distance[i];
+			indexOfmin = i;
+		}
+	}
+	return indexOfmin;
+}
